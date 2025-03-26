3a373d8f307ccd92b680816dea0b5e686416ee8f
abhishekm1929 <abhishekmishra2899@gmail.com>
2025-03-26 13:53:41 +0000

    added job list detail

--- app.py
+++ app.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1, 
+    'title':'Data Analyst', 
+    'location':'Bengaluru , India', 
+    'salary':'Rs.10,00,000',
+  },
+  {
+    'id':2, 
+    'title':'Data Scientist', 
+    'location':'Pune, India', 
+    'salary':'RS.17,00,000',
+  },
+{
+  'id':3, 
+  'title':'Data Engineer', 
+  'location':'Pune, India', 
+  'salary':'Rs.16,00,000',
+},
+  {
+    'id':4, 
+    'title':'Data Engineer', 
+    'location':'San Fransisco, USA', 
+    'salary':'$150k',
+  },
+
+]
 
 @app.route("/")
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html', jobs= JOBS,  
+                         company_name='Jovian')
 
 
 if __name__ == '__main__':
